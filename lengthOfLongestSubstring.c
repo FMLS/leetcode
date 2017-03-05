b127e369384531d0db923871be6abd6b03457430
liuyang <ly04121072@163.com>
2017-03-05 10:47:36 +0800

    modify

--- lengthOfLongestSubstring.c
+++ lengthOfLongestSubstring.c
@@ -10,30 +10,49 @@ int* lengthOfLongestSubstring(char* s) {
                   73, 79, 83, 89, 97, 101};
     int len = strlen(s);
     int i = 0, j = 0;
-    int num = 1;
+    unsigned long long num = 1;
     int now_length = 0;
     int longest = 0;
     int flag = 0;
     int *result = (int*)malloc(sizeof(int) * 2);
-
     for(i = 0; i < len; i++) {
         for (j = i; j < len; j++) {
             int index = s[j] % 26;
-            if (num % primes[index] == 0) {
+            printf("prime: %d , num: %lld\n", primes[index], num);
+            if (s[j] == 'k') {
+                printf("prime: %d\n", primes[index]);
+                printf("cha:%lld \n", num - primes[index]);
+                printf("mo:%lld \n", num % primes[index]);
+
+            }
+
+            if (num > primes[index] && (num % primes[index] == 0)) {
                 num = 1;
-                if (now_length > longest) {
-                    longest = now_length;
-                    flag = j - 1;    
-                }
                 now_length = 0;
+                i = j - 1;
                 break;
             }
+
             //不重复字符计算的数值;
             num *= primes[index];
+//            if (s[j] == 'k') {
+//                printf("%lld", (num / primes[index]) - primes[index]);
+//                printf("num: %lld\n", num);
+//                printf("index: %d\n", index);
+//                printf("mo:%lld", (num /primes[index])%primes[index]);
+//                printf("longest:%d", longest);
+//            }
 
             //现在不重复字母的长度
             now_length = j - i + 1;
+            if (now_length > longest) {
+                longest = now_length;
+                flag = i;    
+            }
         } 
+        if (j == len) {
+            break;
+        }
     }
     result[0] = flag;
     result[1] = longest;
@@ -41,10 +60,15 @@ int* lengthOfLongestSubstring(char* s) {
 }
 
 int main(void) {
-    char a[] = "aaaaabcdee";
+   char a[] = "qvbuatkzwoyxke";
+ //   int i = 0;
+ //   for ( i = 0; i < strlen(a); i++) {
+ //       printf("%c : %d \n", a[i], a[i] % 26);
+ //   }
+
     int *res = lengthOfLongestSubstring(a);
     int i = 0;
-    printf("%d", res[1]);
+    printf("%d\n", res[1]);
     for (i = res[0]; i < res[0] + res[1]; i++) {
         printf("%c", a[i]);
     }
